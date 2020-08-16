97ba86007311e5eb3a8d346167bb320bb7f1fd59
Mionder <49869397+Mionder@users.noreply.github.com>
2020-08-16 16:55:37 +0300

    contacts

---
+++ src/components/Contact/Contact.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+        <div id="region-top">
+		<div class="clearfix">
+			<div class="field-content">
+				<div>
+					<h1>Связаться с нами</h1>
+					<h3>У вас возник вопрос? Мы будем рады вам помочь.</h3>
+
+					<div class="r-calls">
+						<div class="block-title">Прием звонков: </div>
+						<div class="row row-1"><span>Пн - Пт:</span>  08:00 - 20:00</div>
+						<div class="row row-1"><span>Сб - Вс:</span>  08:00 - 17:00</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+	<main>
+		<div class="no-sidebar" id="main-content">
+
+			<section id="sec-2">
+				<div class="layout-center clearfix">
+					<div class="field-content">
+						<div class="row row-1">
+							<div class="field-title">Розничным клиентам</div>
+							<div><a class="btn red popup popup-1" href="#">Отправить запрос</a></div>
+							<div class="ct">
+								<div class="field-label">Консультации</div>
+								<div class="num">
+									<a href="tel:+38 (067) 828-70-07">+38 (067) 828-70-07</a>
+								</div>
+							</div>
+
+							<div class="ct">
+								<div class="field-label">Заказы</div>
+								<div class="num">
+									<a href="tel:+38 (067) 828-70-07">+38 (067) 828-70-07</a>
+									<a href="tel:+38 (067) 828-70-07">+38 (067) 828-70-07</a>
+								</div>
+							</div>
+						</div>
+						<div class="row row-2">
+							<div class="field-title">Оптовым клиентам</div>
+							<div><a class="btn red popup popup-1" href="#">Отправить запрос</a></div>
+							<div class="ct">
+								<div class="field-label">Консультации</div>
+								<div class="num">
+									<a href="tel:+38 (067) 828-70-07">+38 (067) 828-70-07</a>
+								</div>
+							</div>
+							<div class="ct">
+								<div class="field-label">B2B центр</div>
+								<div class="to-link">
+									<a href="#">Перейти на портал</a>
+								</div>
+							</div>
+
+						</div>
+						<div class="row row-3">
+							<div class="field-title">Сервисный центр</div>
+							<div><a class="btn red popup popup-1" href="#">Отправить запрос</a></div>
+							<div class="ct">
+								<div class="field-label">По всем вопросам</div>
+								<div class="num">
+									<a href="tel:+38 (067) 828-70-07">+38 (067) 828-70-07</a>
+								</div>
+							</div>
+
+							<div class="ct">
+								<div class="field-label">Адрес<br>г. Харьков, Московский просп., 247</div>
+								<div class="to-link">
+									<a href="#">Посмотреть на карте</a>
+								</div>
+							</div>
+						</div>
+						<div class="row row-4">
+							<div class="field-title">Общие вопросы</div>
+							<div><a class="btn red popup popup-1" href="#">Отправить запрос</a></div>
+							<div class="ct">
+								<div class="field-label">По всем вопросам</div>
+								<div class="num">
+									<a href="tel:+38 (067) 828-70-07">+38 (067) 828-70-07</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="sec-3">
+				<div class="layout-center clearfix">
+					<div class="field-content">
+						<div class="title">
+							Будьте в теме
+						</div>
+						<p>Все самое актуальное от intertool.ua</p>
+						<br>
+						<br>
+						<div class="soc">
+							<a class="you" href="#"></a>
+							<a class="fb" href="#"></a>
+						</div>
+					</div>	
+				</div>			
+			</section>
+		</div>	
+	</main>
+    </div>
+</template>
+
+<script>
+import "./contact.scss";
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import Article from "./components/Article/Article.vue";
 import About from "./components/About/About.vue";
 import Account from "./components/Account/Account.vue";
 import Order from "./components/Order/Order.vue";
+import Contact from "./components/Contact/Contact.vue";
 
 const routes = [
   { path: "/", component: Article },
   { path: "/about", component: About },
   { path: "/account", component: Account },
-  { path: "/order", component: Order}
+  { path: "/order", component: Order},
+  { path: "/contact", component: Contact}
 ];
 
 const router = new VueRouter({
