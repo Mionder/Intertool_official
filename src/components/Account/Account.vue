1135ecc960d80ff214f1e389f0e3c38a50ef99a9
teemaYouyoh <mrjokerok21@gmail.com>
2020-08-16 15:49:55 +0300

    account_page

---
+++ src/components/Account/Account.vue
@@ -0,0 +1,458 @@
+<template>
+  <div>
+    <main class="two-column">
+      <div class="layout-center clearfix">
+        <h1>Личный кабинет</h1>
+
+        <div class="mobile bonuses">
+          <div class="bonus">
+            <i class="tooltip_info_btn tooltip_info_btn_white"><span></span></i>
+            <div class="tooltip">
+              <div class="close"></div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+              neque repudiandae nesciunt qui natus nihil voluptatum, sapiente
+              impedit itaque libero quasi molestiae error fuga, ipsum fugiat
+              aspernatur laboriosam temporibus, beatae ab eaque id ullam eum?
+              Unde doloremque est reiciendis possimus voluptates, ab, facere,
+              veritatis cum explicabo inventore quasi saepe commodi!
+            </div>
+            Ваши бонусы: <span class="sp-bonus">58978</span>
+          </div>
+          <div class="savings">
+            <i class="tooltip_info_btn tooltip_info_btn_white"><span></span></i>
+            <div class="tooltip">
+              <div class="close"></div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+              neque repudiandae nesciunt qui natus nihil voluptatum, sapiente
+              impedit itaque libero quasi molestiae error fuga, ipsum fugiat
+              aspernatur laboriosam temporibus, beatae ab eaque id ullam eum?
+              Unde doloremque est reiciendis possimus voluptates, ab, facere,
+              veritatis cum explicabo inventore quasi saepe commodi!
+            </div>
+            Ваша экономия: <span class="sp-savings">22 895 грн</span>
+          </div>
+        </div>
+
+        <div class="content">
+          <div id="account-info">
+            <div class="left">
+              <div class="greeting">
+                <span>Здравствуйте,</span> <a href="">Иван Иванов</a>
+              </div>
+              <div class="status">
+                <span>Ваш Статус:</span><a href=""> Бронзовый</a>
+                <i class="tooltip_info_btn tooltip_info_btn_black"
+                  ><span></span
+                ></i>
+                <div class="tooltip">
+                  <div class="close"></div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                  neque repudiandae nesciunt qui natus nihil voluptatum,
+                  sapiente impedit itaque libero quasi molestiae error fuga,
+                  ipsum fugiat aspernatur laboriosam temporibus, beatae ab eaque
+                  id ullam eum? Unde doloremque est reiciendis possimus
+                  voluptates, ab, facere, veritatis cum explicabo inventore
+                  quasi saepe commodi!
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="savings">
+                <i class="tooltip_info_btn tooltip_info_btn_white"
+                  ><span></span
+                ></i>
+                <div class="tooltip">
+                  <div class="close"></div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                  neque repudiandae nesciunt qui natus nihil voluptatum,
+                  sapiente impedit itaque libero quasi molestiae error fuga,
+                  ipsum fugiat aspernatur laboriosam temporibus, beatae ab eaque
+                  id ullam eum? Unde doloremque est reiciendis possimus
+                  voluptates, ab, facere, veritatis cum explicabo inventore
+                  quasi saepe commodi!
+                </div>
+                Ваша экономия: <span class="sp-savings">22 895 грн</span>
+              </div>
+              <div class="bonus">
+                <i class="tooltip_info_btn tooltip_info_btn_white"
+                  ><span></span
+                ></i>
+                <div class="tooltip">
+                  <div class="close"></div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                  neque repudiandae nesciunt qui natus nihil voluptatum,
+                  sapiente impedit itaque libero quasi molestiae error fuga,
+                  ipsum fugiat aspernatur laboriosam temporibus, beatae ab eaque
+                  id ullam eum? Unde doloremque est reiciendis possimus
+                  voluptates, ab, facere, veritatis cum explicabo inventore
+                  quasi saepe commodi!
+                </div>
+                Ваши бонусы: <span class="sp-bonus">58978</span>
+              </div>
+            </div>
+          </div>
+
+          <div id="account-link">
+            <div class="row row-1">
+              <a href="">
+                <div class="field-img">
+                  <img src="dist/images/icon20.png" alt="" />
+                </div>
+                <div>
+                  <span class="title">Ваши заказы</span>
+                  <span class="inf"
+                    >Отслеживать, возвращать или покупать вещи снова</span
+                  >
+                </div>
+              </a>
+            </div>
+            <div class="row row-2">
+              <a href="">
+                <div class="field-img">
+                  <img src="dist/images/icon21.png" alt="" />
+                </div>
+                <div>
+                  <span class="title">Логин и безопасность</span>
+                  <span class="inf"
+                    >Изменить логин, имя и номер мобильного телефона</span
+                  >
+                </div>
+              </a>
+            </div>
+            <div class="row row-3">
+              <a href="">
+                <div class="field-img">
+                  <img src="dist/images/icon22.png" alt="" />
+                </div>
+                <div>
+                  <span class="title">Ваши адреса</span>
+                  <span class="inf"
+                    >Редактировать адреса для заказов и подарков</span
+                  >
+                </div>
+              </a>
+            </div>
+            <div class="row row-4">
+              <a href="">
+                <div class="field-img">
+                  <img src="dist/images/icon23.png" alt="" />
+                </div>
+                <div>
+                  <span class="title">Варианты оплаты</span>
+                  <span class="inf">Изменить или добавить способы оплаты</span>
+                </div>
+              </a>
+            </div>
+            <div class="row row-5">
+              <a href="">
+                <div class="field-img">
+                  <img src="dist/images/icon24.png" alt="" />
+                </div>
+                <div>
+                  <span class="title">Избранные товары</span>
+                  <span class="inf"
+                    >Отслеживать, возвращать или покупать вещи снова</span
+                  >
+                </div>
+              </a>
+            </div>
+
+            <div class="row row-7">
+              <a href="">
+                <div class="field-img">
+                  <img src="dist/images/icon26.png" alt="" />
+                </div>
+                <div>
+                  <span class="title">Мои промокоды</span>
+                  <span class="inf">
+                    <ul>
+                      <li>1s3asEw2</li>
+                      <li>1s3asEw2</li>
+                      <li>1s3asEw2</li>
+                    </ul>
+                  </span>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div id="another-link">
+            <div class="row row-1">
+              <div class="block-title">Покупателю</div>
+              <ul>
+                <li><a href="">Оплата </a></li>
+                <li><a href="">Доставка</a></li>
+                <li><a href="">Частые вопросы</a></li>
+                <li><a href="">Сервесные центры</a></li>
+                <li><a href="">Гарантия</a></li>
+                <li><a href="">Подарочные сертификаты</a></li>
+              </ul>
+            </div>
+            <div class="row row-2">
+              <div class="block-title">Сотрудничество</div>
+              <ul>
+                <li><a href="">Оплата </a></li>
+                <li><a href="">Доставка</a></li>
+                <li><a href="">Частые вопросы</a></li>
+                <li><a href="">Сервесные центры</a></li>
+                <li><a href="">Гарантия</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <div id="triptych-0">
+      <div class="layout-center clearfix">
+        <div id="slider-gray">
+          <div class="wrap" id="frame">
+            <!-- <div class="block-title">Выгодные предложения</div> -->
+            <ul class="field-content" id="slide-progress">
+              <li class="row row-1">
+                <div class="content">
+                  <div class="tov tov-1">
+                    <div class="field field-img">
+                      <img src="dist/images/tov1.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Компрессор 50 л, 3 кВт, 220 В, 8 атм, 420 л/мин, 2
+                        цилиндра. INTERTOOL ...
+                      </div>
+                      <div class="field-price">
+                        2 399 грн.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="plus"></div>
+                  <div class="tov tov-2">
+                    <div class="field-img">
+                      <img src="dist/images/tov2.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Краскопульт пневматический LVLP BRONZE профессиональный,
+                        форсунка 1.8мм
+                      </div>
+                      <div class="field-price">
+                        849 грн.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="content content-sum">
+                  <div class="field old-price">688 грн.</div>
+                  <div class="field price">4000 грн.</div>
+                  <div class="field saving">
+                    Экономия: <span>-300 грн.</span>
+                  </div>
+                  <div class="field add-cart">
+                    <a href="" class="btn red">Купить вместе</a>
+                  </div>
+                </div>
+              </li>
+
+              <li class="row row-1">
+                <div class="content">
+                  <div class="tov tov-1">
+                    <div class="field field-img">
+                      <img src="dist/images/tov1.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Компрессор 50 л, 3 кВт, 220 В, 8 атм, 420 л/мин, 2
+                        цилиндра. INTERTOOL ...
+                      </div>
+                      <div class="field-price">
+                        2 399 грн.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="plus"></div>
+                  <div class="tov tov-2">
+                    <div class="field-img">
+                      <img src="dist/images/tov2.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Краскопульт пневматический LVLP BRONZE профессиональный,
+                        форсунка 1.8мм
+                      </div>
+                      <div class="field-price">
+                        849 грн.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="content content-sum">
+                  <div class="field old-price">688 грн.</div>
+                  <div class="field price">4000 грн.</div>
+                  <div class="field saving">
+                    Экономия: <span>-300 грн.</span>
+                  </div>
+                  <div class="field add-cart">
+                    <a href="" class="btn red">Купить вместе</a>
+                  </div>
+                </div>
+              </li>
+
+              <li class="row row-1">
+                <div class="content">
+                  <div class="tov tov-1">
+                    <div class="field field-img">
+                      <img src="dist/images/tov1.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Компрессор 50 л, 3 кВт, 220 В, 8 атм, 420 л/мин, 2
+                        цилиндра. INTERTOOL ...
+                      </div>
+                      <div class="field-price">
+                        2 399 грн.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="plus"></div>
+                  <div class="tov tov-2">
+                    <div class="field-img">
+                      <img src="dist/images/tov2.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Краскопульт пневматический LVLP BRONZE профессиональный,
+                        форсунка 1.8мм
+                      </div>
+                      <div class="field-price">
+                        849 грн.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="content content-sum">
+                  <div class="field old-price">688 грн.</div>
+                  <div class="field price">4000 грн.</div>
+                  <div class="field saving">
+                    Экономия: <span>-300 грн.</span>
+                  </div>
+                  <div class="field add-cart">
+                    <a href="" class="btn red">Купить вместе</a>
+                  </div>
+                </div>
+              </li>
+
+              <li class="row row-1">
+                <div class="content">
+                  <div class="tov tov-1">
+                    <div class="field field-img">
+                      <img src="dist/images/tov1.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Компрессор 50 л, 3 кВт, 220 В, 8 атм, 420 л/мин, 2
+                        цилиндра. INTERTOOL ...
+                      </div>
+                      <div class="field-price">
+                        2 399 грн.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="plus"></div>
+                  <div class="tov tov-2">
+                    <div class="field-img">
+                      <img src="dist/images/tov2.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Краскопульт пневматический LVLP BRONZE профессиональный,
+                        форсунка 1.8мм
+                      </div>
+                      <div class="field-price">
+                        849 грн.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="content content-sum">
+                  <div class="field old-price">688 грн.</div>
+                  <div class="field price">4000 грн.</div>
+                  <div class="field saving">
+                    Экономия: <span>-300 грн.</span>
+                  </div>
+                  <div class="field add-cart">
+                    <a href="" class="btn red">Купить вместе</a>
+                  </div>
+                </div>
+              </li>
+
+              <li class="row row-1">
+                <div class="content">
+                  <div class="tov tov-1">
+                    <div class="field field-img">
+                      <img src="dist/images/tov1.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Компрессор 50 л, 3 кВт, 220 В, 8 атм, 420 л/мин, 2
+                        цилиндра. INTERTOOL ...
+                      </div>
+                      <div class="field-price">
+                        2 399 грн.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="plus"></div>
+                  <div class="tov tov-2">
+                    <div class="field-img">
+                      <img src="dist/images/tov2.png" />
+                    </div>
+                    <div class="field field-body">
+                      <div class="field-text">
+                        Краскопульт пневматический LVLP BRONZE профессиональный,
+                        форсунка 1.8мм
+                      </div>
+                      <div class="field-price">
+                        849 грн.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="content content-sum">
+                  <div class="field old-price">688 грн.</div>
+                  <div class="field price">4000 грн.</div>
+                  <div class="field saving">
+                    Экономия: <span>-300 грн.</span>
+                  </div>
+                  <div class="field add-cart">
+                    <a href="" class="btn red">Купить вместе</a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="c-scrollbar">
+            <div class="c-scrollbar__handle">
+              <div class="c-scrollbar__handle-mousearea"></div>
+            </div>
+          </div>
+
+          <div class="controls center">
+            <button class="btn prev">
+              <i class="icon-chevron-left"></i>
+            </button>
+
+            <button class="btn next">
+              <i class="icon-chevron-right"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./account.scss";
+export default {};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import VueRouter from "vue-router";
 
 import Article from "./components/Article/Article.vue";
 import About from "./components/About/About.vue";
+import Account from "./components/Account/Account.vue";
 
 const routes = [
   { path: "/", component: Article },
   { path: "/about", component: About },
+  { path: "/account", component: Account },
 ];
 
 const router = new VueRouter({
